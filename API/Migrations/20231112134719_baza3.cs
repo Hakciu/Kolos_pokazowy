cd493d560f59d98dc75fda21b52234db80717cd5
Jakub Janik <JJanik2k@gmail.com>
2023-11-12 14:51:14 +0100

    update

---
+++ API/Migrations/20231112134719_baza3.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class baza3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "origin",
+                table: "People",
+                newName: "Origin");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Origin",
+                table: "People",
+                newName: "origin");
+        }
+    }
+}
